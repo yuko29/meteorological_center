580bd94734a0e0f2c6f29af0fd28fdf5ff635773
_311551169_劉宇承 <laxiflora@LAPTOP-95KBHVNS.localdomain>
2023-05-20 19:18:03 +0800

    update_insert_api

--- connect.py
+++ connect.py
@@ -1,5 +1,13 @@
 from mongoDB import mongoDB
 
+earthEqake_test = {'time':"2023-5-12 03:40:52", 'M_L':3.6, 'focal_dep': 3.2, 'longitude': 41.0, 'latitude': 20.7}
+electricity_test = {'region':"北", 'power_usage':512.3, 'power_generate': 482.1, 'time': "2023-5-12 03:40:52"}
+reservoir_test = {'time':"2023-5-12 03:40:52", 'percentage': 42.6, 'water_supply': 321.2, 'name': "德基水庫"}
+
+
 a = mongoDB()
 a.exampleAPI()
 a.exampleAPI2()
+a.insertEarthquake(earthEqake_test)
+a.insertElectricity(electricity_test)
+a.insertReservoir(reservoir_test)
--- mongoDB.py
+++ mongoDB.py
@@ -3,10 +3,105 @@ import pymongo
 class mongoDB():
     def __init__(self):
         # Create a MongoClient object with the IP address of the MongoDB container
-        self.client = pymongo.MongoClient("mongodb://172.30.58.128:27017/")
-
+        self.client = pymongo.MongoClient("mongodb://172.28.138.245:2047/")
         # Connect to the "mydatabase" database
-        self.db = self.client["mydatabase"]
+        self.db = self.client["meteorological_center"]
+        self.earthEqake = self.db['earthQuake_latest']
+        self.reservoir = self.db['reservoir']
+        self.electricity = self.db['electricity']
+
+    def __dataValid(self, data, functName):
+        if(functName == self.insertEarthquake.__name__):
+            return type(data['time']) == str\
+                    and type(data['M_L']) == float\
+                    and type(data['focal_dep']) == float\
+                    and type(data['longitude']) == float\
+                    and type(data['latitude']) == float
+
+        if(functName == self.insertReservoir.__name__):
+            return type(data['name']) == str\
+                    and type(data['water_supply']) == float\
+                    and type(data['percentage']) == float\
+                    and type(data['time']) == str\
+        
+        if(functName == self.insertElectricity.__name__):
+            return type(data['region']) == str\
+                    and type(data['power_usage']) == float\
+                    and type(data['power_generate']) == float\
+                    and type(data['time']) == str
+
+    def insertEarthquake(self, data):
+        print(data)
+        try:
+            assert self.__dataValid(data, self.insertEarthquake.__name__) == True
+            self.earthEqake.insert_one(data)
+            return 0
+        except KeyError as e:
+            print(f"{e.__class__.__name__} on checking column {e.args}")
+            return 1
+        except Exception as e:
+            print(f"{e.__class__.__name__} occured.")
+            return 1
+
+    def insertReservoir(self, data):
+        print(data)
+        try:
+            assert self.__dataValid(data, self.insertReservoir.__name__) == True
+            self.reservoir.insert_one(data)
+            return 0
+        except KeyError as e:
+            print(f"{e.__class__.__name__} on checking column {e.args}")
+            return 1
+        except Exception as e:
+            print(f"{e.__class__.__name__} occured.")
+            return 1
+
+    def insertElectricity(self, data):
+        print(data)
+        try:
+            assert self.__dataValid(data, self.insertElectricity.__name__) == True
+            self.reservoir.insert_one(data)
+            return 0
+        except KeyError as e:
+            print(f"{e.__class__.__name__} on checking column {e.args}")
+            return 1
+        except Exception as e:
+            print(f"{e.__class__.__name__} occured.")
+            return 1
+	
+		
+
+# const session = db.getMongo().startSession();
+# session.startTransaction();
+
+# try {
+#   // Read the documents and count the size within the transaction
+#   const documents = db.yourCollectionName.find({}, { projection: { _id: 1 } }).toArray();
+#   const collectionSize = documents.length;
+
+#   if (collectionSize > 100) {
+#     // Sort the documents by _id and delete the oldest document
+#     const oldestDocument = db.yourCollectionName.findOneAndDelete(
+#       {},
+#       { sort: { _id: 1 }, collation: { locale: "en_US", numericOrdering: true } }
+#     );
+
+#     // Commit the transaction
+#     session.commitTransaction();
+
+#     // Use the `oldestDocument` if needed for further processing or logging
+#   } else {
+#     // Abort the transaction if the collection size is not greater than 100
+#     session.abortTransaction();
+#   }
+# } finally {
+#   // End the transaction and release the session
+#   session.endSession();
+# }
+
+
+
+
 
 
     def exampleAPI(self):
@@ -22,3 +117,4 @@ class mongoDB():
         # Find all documents in the collection
         for document in col.find():
             print(document)
+        
