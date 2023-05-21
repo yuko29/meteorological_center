a4909e15b8e7fd3566c566d171dc527263b4fc72
vivian96385 <70514684+vivian96385@users.noreply.github.com>
2023-05-21 11:58:23 +0800

    package to dict type, add calculation

--- crawling_water.py
+++ crawling_water.py
@@ -1,16 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
+import locale
+
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 water = requests.get("https://fhy.wra.gov.tw/ReservoirPage_2011/Statistics.aspx") #將此頁面的HTML GET下來
 #print(water.text) #印出HTML
 
 soup = BeautifulSoup(water.text, 'html.parser')
-#title_tag = soup.title
+
+dict1={}
 re_list = ["石門水庫", "寶山第二水庫", "永和山水庫", "鯉魚潭水庫", "德基水庫", "南化水庫", "曾文水庫", "烏山頭水庫"]
 for re in re_list:
     stw = soup.find('a', string = re).find_parent().find_parent().find_all("td")
     print(re)
-    print("水情時間: " + stw[1].string) # 水情時間
-    print("有效蓄水量: " + stw[6].string) # 有效蓄水量
-    print("蓄水百分比: " + stw[7].string) # 蓄水百分比
+    
+    water_time = str(stw[1].string)
+    print("水情時間: ", water_time) # 水情時間
+    
+    water_avail = locale.atof(stw[6].string)
+    print("有效蓄水量: ", water_avail) # 有效蓄水量
+    
+    water_per = str(stw[7].string).replace(" %","")
+    water_per = float(water_per)
+    print("蓄水百分比: ", water_per) # 蓄水百分比
+    
+    dict1["re"] = {"water_time": "water_time", "water_avail": water_avail, "water_per": water_per}
+
     print()
+
+print("放完測試")
+print(dict1)
