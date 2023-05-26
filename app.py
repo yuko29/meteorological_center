fe602ce3f82d4e95a1379b966d8c9363e607f400
Max <70897987+410011max@users.noreply.github.com>
2023-05-26 16:50:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -84,29 +84,9 @@ def tainan():
     return jsonify({'data': tainan_data})
 
 
-# # 傳送地震資料給 frontend
-# @app.route('/send_earthquake_data', methods=['POST'])
-# def send_earthquake_data():
-#     earthquake_data = get_earthquake_data()
-#     return jsonify(earthquake_data)
-
-# # 傳送水庫資料給 frontend
-# @app.route('/send_reservoir_data', methods=['POST'])
-# def send_reservoir_data():
-#     reservoir_data = get_reservoir_data
-#     return jsonify(reservoir_data)
-
-# # 傳送供電資料給 frontend
-# @app.route('/send_power_data', methods=['POST'])
-# def send_power_data():
-#     power_data = get_power_data
-#     return jsonify(power_data)
-
-
 #-------------------------       database       -------------------------#
 
 db = mongoDB()
-
 
 earthEqake_test = {'time': '2023-5-12 03:40:52', 'M_L': 3.6, 'focal_dep': 3.2, 'longitude': 41.0, 'latitude': 20.7, 'magnitude': [{'factory': '竹', 'magnitude': 0.00032644588703523386}, {'factory': '中', 'magnitude': 0.00019905740360289052}, {'factory': '南', 'magnitude': 0.0003714193582435097}]}
 earthEqake_test2 = {'time': '2023-5-12 03:40:52', 'M_L': 3.6, 'focal_dep': 3.2, 'longitude': 41.0, 'latitude': 20.7, 'magnitude': [{'factory': '竹', 'magnitude': 0.00032644588703523386}, {'factory': '中', 'magnitude': 0.00019905740360289052}, {'factory': '南', 'magnitude': 0.0003714193582435097}]}
@@ -122,50 +102,6 @@ db.insertElectricity(electricity_test2)
 db.insertReservoir(reservoir_test)
 db.insertReservoir(reservoir_test2)
 
-# 配置 MongoDB 連接
-# app.config['MONGO_URI'] = 'mongodb://localhost:27017/mydatabase'
-# mongo = PyMongo(app)
-
-# # 向 DB 拿地震資料
-# @app.route('/earthquake', methods=['GET'])
-# def get_earthquake_data():
-#     earthquake_data = mongo.db.earthquake.find()
-#     result = []
-#     for data in earthquake_data:
-#         result.append({
-#             '地震時間': data['地震時間'],
-#             '規模(M_L)': data['規模(M_L)'],
-#             '地震深度(focal dep)': data['地震深度(focal dep)'],
-#             '經度(東)(longitude)': data['經度(東)(longitude)'],
-#             '緯度(北)(latitude)': data['緯度(北)(latitude)']
-#         })
-#     return jsonify(result)
-
-# # 向 DB 拿水庫資料
-# @app.route('/reservoir', methods=['GET'])
-# def get_reservoir_data():
-#     reservoir_data = mongo.db.reservoir.find()
-#     result = []
-#     for data in reservoir_data:
-#         result.append({
-#             '水情時間': data['水情時間'],
-#             '有效蓄水量': data['有效蓄水量'],
-#             '蓄水百分比': data['蓄水百分比']
-#         })
-#     return jsonify(result)
-
-# # 向 DB 拿供電資料
-# @app.route('/power', methods=['GET'])
-# def get_power_data():
-#     power_data = mongo.db.power.find()
-#     result = []
-#     for data in power_data:
-#         result.append({
-#             '即時發電量': data['即時發電量'],
-#             '即時用電量': data['即時用電量'],
-#             '供電百分比': data['供電百分比']
-#         })
-#     return jsonify(result)
 
 
 if __name__ == '__main__':
