37cf1ee50615a47553767b3d18a7c1b26c2487ee
vivian96385 <70514684+vivian96385@users.noreply.github.com>
2023-05-26 16:42:23 +0800

    Insert DB ok

--- crawling_water.py
+++ crawling_water.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import locale
+from mongoDB import mongoDB
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
@@ -9,6 +10,7 @@ water = requests.get("https://fhy.wra.gov.tw/ReservoirPage_2011/Statistics.aspx"
 soup = BeautifulSoup(water.text, 'html.parser')
 
 reservoir = {}
+a = mongoDB()
 re_list = ["石門水庫", "寶山第二水庫", "永和山水庫", "鯉魚潭水庫", "德基水庫", "南化水庫", "曾文水庫", "烏山頭水庫"]
 for re in re_list:
     stw = soup.find('a', string = re).find_parent().find_parent().find_all("td")
@@ -18,23 +20,22 @@ for re in re_list:
         water_time = '-'
     else:
         water_time = str(stw[1].string)
-    #print("水情時間: ", water_time) # 水情時間
     
     if stw[6].string == "--":
         water_avail = -1.0
     else:
         water_avail = locale.atof(stw[6].string)
-    #print("有效蓄水量: ", water_avail) # 有效蓄水量
     
     if stw[7].string == "--":
         water_per = -1.0
     else:
         water_per = str(stw[7].string).replace(" %","")
         water_per = float(water_per)
-    #print("蓄水百分比: ", water_per) # 蓄水百分比
     
-    reservoir[re] = {"water_time": water_time, "water_avail": water_avail, "water_per": water_per}
+    reservoir[re] = {"time": water_time, "volume": water_avail, "percentage": water_per}
+    a.insertReservoir({'time':water_time, 'percentage': water_per, 'water_supply': water_avail, 'name': re})
 
-    #print()
 
 print(reservoir)
+
+#reservoir_test2 = {'time':"2023-5-12 03:40:52", 'percentage': 42.6, 'water_supply': 321.2, 'name': "德基水庫"}
