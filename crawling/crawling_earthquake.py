b03a089116b123c4b9908ee76d6f1ba93123c0d6
vivian96385 <70514684+vivian96385@users.noreply.github.com>
2023-06-02 16:18:50 +0800

    Add files via upload

--- crawling/crawling_earthquake.py
+++ crawling/crawling_earthquake.py
@@ -2,17 +2,21 @@ import requests
 import re
 import math
 from math import sin, cos, radians
-from dbAPI.MongoDB import MongoDB
+#from dbAPI.MongoDB import MongoDB
 
 
-def crawl_data_ten():
-    earthquake = requests.get("https://scweb.cwb.gov.tw/") #將此頁面的HTML GET下來
+def get_history(url: str):
+    earthquake = requests.get(url) # get HTML
     html_content = earthquake.content.decode('utf-8')
 
-    pattern = re.compile(r'var\s+locations\s*=\s*([^;]+);')
-    matches = pattern.findall(html_content)
-    history = eval(matches[0])
+    pattern = re.compile(r'var\s+locations\s*=\s*([^;]+);') # format of "var location"
+    matches = pattern.findall(html_content) # find content in html that matches format
+    history = eval(matches[0]) # split to a list
+    
+    return history
 
+def crawl_ten_data(url: str):
+    history = get_history(url)
     history_list = []
     for i in range(10):
         history_list.append({'time':history[i][2], 'M_L':history[i][3], 'focal_dep': history[i][4], 'longitude': float(history[i][7]), 'latitude': float(history[i][8])})
@@ -21,20 +25,13 @@ def crawl_data_ten():
     return history_list
 
 
-def crawl_data():
-    earthquake = requests.get("https://scweb.cwb.gov.tw/") #將此頁面的HTML GET下來
-    #print(earthquake.text) #印出HTML
-    html_content = earthquake.content.decode('utf-8')
-
-    pattern = re.compile(r'var\s+locations\s*=\s*([^;]+);')
-    matches = pattern.findall(html_content)
-    history = eval(matches[0])
-
+def crawl_data(url: str):
+    history = get_history(url)
     earthQuake = {'time':history[0][2], 'M_L':history[0][3], 'focal_dep': history[0][4], 'longitude': float(history[0][7]), 'latitude': float(history[0][8])}
     return earthQuake
     #earthEqake_test = {'time':"2023-5-12 03:40:52", 'M_L':3.6, 'focal_dep': 3.2, 'longitude': 41.0, 'latitude': 20.7}
 
-def longitude_difference_to_km(longitude1, longitude2):
+def longitude_difference_to_km(longitude1: float, longitude2: float):
     """
     Convert the difference between two longitudes into kilometers using the haversine formula.
     
@@ -62,7 +59,7 @@ def longitude_difference_to_km(longitude1, longitude2):
     return abs(distance)
 
 
-def latitude_difference_to_km(latitude1, latitude2):
+def latitude_difference_to_km(latitude1: float, latitude2: float):
     """
     Convert the difference between two latitudes into kilometers.
     
@@ -87,16 +84,11 @@ def latitude_difference_to_km(latitude1, latitude2):
     
     return abs(distance)
 
-def getDistance(x, y):
+def getDistance(x: float, y: float):
     return math.sqrt(x**2 + y**2)
 
-GG_factory = [
-    {'factory': '竹', 'longitude': 121.01, 'latitude': 24.773, 'Si': 1.758, 'Padj': 1.0, 'magnitude': []},
-    {'factory': '中', 'longitude': 120.618, 'latitude': 24.2115, 'Si': 1.063, 'Padj': 1.0, 'magnitude': []},
-    {'factory': '南', 'longitude': 120.272, 'latitude': 23.1135, 'Si': 1.968, 'Padj': 1.0, 'magnitude': []}
-]
 
-def calculate_magnitude(data):
+def calculate_magnitude(data: dict, GG_factory: list):
     fac_magnitude = []
     for fac in GG_factory:
         long_km = latitude_difference_to_km(fac['longitude'], data['longitude'])
@@ -119,20 +111,28 @@ def calculate_magnitude(data):
     data['magnitude'] = fac_magnitude
     return(data)
 
-def get_new_update():
-    crawled = crawl_data()
+def get_new_update(url: str):
+    crawled = crawl_data(url)
     eq_with_mag = calculate_magnitude(crawled)
 
+def main():
+    url = "https://scweb.cwb.gov.tw/"
+    GG_factory = [
+    {'factory': '竹', 'longitude': 121.01, 'latitude': 24.773, 'Si': 1.758, 'Padj': 1.0, 'magnitude': []},
+    {'factory': '中', 'longitude': 120.618, 'latitude': 24.2115, 'Si': 1.063, 'Padj': 1.0, 'magnitude': []},
+    {'factory': '南', 'longitude': 120.272, 'latitude': 23.1135, 'Si': 1.968, 'Padj': 1.0, 'magnitude': []}
+    ]
+    
+    
+    earthQuake_list = crawl_ten_data(url)
+
+    for earthQuake in earthQuake_list:
+        earthQuake = calculate_magnitude(earthQuake, GG_factory)
+    #print(earthQuake_list)
+    
+    #a = MongoDB()
+    #a.insert_earthquake_data(earthQuake_list)
 
 
-earthQuake_list = crawl_data_ten()
-for earthQuake in earthQuake_list:
-    earthQuake = calculate_magnitude(earthQuake)
-a = MongoDB()
-#a.insert_earthquake_data(earthEqake_list)
-#a.insert_earthquake_data(earthQuake_list[0])
-a.insert_earthquake_data(earthQuake_list)
-
-#{'time': '2023-5-12 03:40:52', 'M_L': 3.6, 'focal_dep': 3.2, 'longitude': 41.0, 'latitude': 20.7, 'magnitude': [{'factory': '竹', 'magnitude': 0.00032644588703523386}, {'factory': '中', 'magnitude': 0.00019905740360289052}, {'factory': '南', 'magnitude': 0.0003714193582435097}]}
-
-#print(earthQuake_list)
+if __name__ == "__main__":
+    main()
