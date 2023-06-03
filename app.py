77b0de24893b7b894064ab8762f328bbf2f9bf70
410011max <410011max@gmail.com>
2023-06-03 21:04:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def toggle_menu():
 # 竹(石門水庫、寶山第二水庫、永和山水庫)
 hsinchu_data = {
     "Electricity_value": 2000, "Electricity_maximum": 3000,
-    "Earthquake_time": "2022/5/23 下午 6:23:00", "Earthquake_magnitude": "1強",
+    "Earthquake_time": "2022/5/23 下午 6:23:00", "Earthquake_magnitude": "1",
     "Reservoir_name_1": "石門水庫", "Reservoir_percentage_1": f"{40}%",
     "Reservoir_name_2": "寶山第二水庫", "Reservoir_percentage_2": f"{60}%",
     "Reservoir_name_3": "永和山水庫", "Reservoir_percentage_3": f"{80}%"
@@ -40,7 +40,7 @@ hsinchu_data = {
 # 中(鯉魚潭水庫、德基水庫)
 taichung_data = {
     "Electricity_value": 1000, "Electricity_maximum": 2000,
-    "Earthquake_time": "2022/5/24 下午 6:24:00", "Earthquake_magnitude": "2弱",
+    "Earthquake_time": "2022/5/24 下午 6:24:00", "Earthquake_magnitude": "2",
     "Reservoir_name_1": "鯉魚潭水庫", "Reservoir_percentage_1": f"{10}%",
     "Reservoir_name_2": "德基水庫", "Reservoir_percentage_2": f"{20}%",
     "Reservoir_name_3": "", "Reservoir_percentage_3": ""
@@ -49,7 +49,7 @@ taichung_data = {
 # 南(南化水庫、曾文水庫、烏山頭水庫)
 tainan_data = {
     "Electricity_value": 500, "Electricity_maximum": 1000,
-    "Earthquake_time": "2022/5/25 下午 6:25:00", "Earthquake_magnitude": "3強",
+    "Earthquake_time": "2022/5/25 下午 6:25:00", "Earthquake_magnitude": "3",
     "Reservoir_name_1": "南化水庫", "Reservoir_percentage_1": f"{1}%",
     "Reservoir_name_2": "曾文水庫", "Reservoir_percentage_2": f"{2}%",
     "Reservoir_name_3": "烏山頭水庫", "Reservoir_percentage_3": f"{3}%"
@@ -61,12 +61,12 @@ def hsinchu():
     print("click hsinchu !")
     
     # earthquuake
-    earthquake_data = db.retrieveEarthquake(1)[0]
+    earthquake_data = db.retrieveFactoryEarthquake(1, "竹")[0]
     hsinchu_data["Earthquake_time"] = earthquake_data["time"]
-    hsinchu_data["Earthquake_magnitude"] = earthquake_data["M_L"]
+    hsinchu_data["Earthquake_magnitude"] = earthquake_data["magnitude"]
     
     # electricity
-    electricity_data = db.retrieveElectricity(1, '北')[0]
+    electricity_data = db.retrieveElectricity(1, '竹')[0]
     hsinchu_data["Electricity_value"] = electricity_data["power_usage"]
     hsinchu_data["Electricity_maximum"] = electricity_data["power_generate"]
     
@@ -86,9 +86,9 @@ def taichung():
     print("click taichung !")
     
     # earthquuake
-    earthquake_data = db.retrieveEarthquake(1)[0]
+    earthquake_data = db.retrieveFactoryEarthquake(1, "中")[0]
     taichung_data["Earthquake_time"] = earthquake_data["time"]
-    taichung_data["Earthquake_magnitude"] = earthquake_data["M_L"]
+    taichung_data["Earthquake_magnitude"] = earthquake_data["magnitude"]
     
     # electricity
     electricity_data = db.retrieveElectricity(1, '中')[0]
@@ -109,12 +109,13 @@ def tainan():
     print("click tainan !")
 
     # earthquuake
-    earthquake_data = db.retrieveEarthquake(1)[0]
+    earthquake_data = db.retrieveFactoryEarthquake(1, "南")[0]
     tainan_data["Earthquake_time"] = earthquake_data["time"]
-    tainan_data["Earthquake_magnitude"] = earthquake_data["M_L"]
+    tainan_data["Earthquake_magnitude"] = earthquake_data["magnitude"]
+    db.retrieveFactoryEarthquake
     
     # electricity
-    electricity_data = db.retrieveElectricity(1, '北')[0]
+    electricity_data = db.retrieveElectricity(1, "南")[0]
     tainan_data["Electricity_value"] = electricity_data["power_usage"]
     tainan_data["Electricity_maximum"] = electricity_data["power_generate"]
     
@@ -135,7 +136,7 @@ db = mongoDB()
 
 earthEqake_test = {'time': '2023-5-12 03:40:52', 'M_L': 3.6, 'focal_dep': 3.2, 'longitude': 41.0, 'latitude': 20.7, 'magnitude': [{'factory': '竹', 'magnitude': 0.00032644588703523386}, {'factory': '中', 'magnitude': 0.00019905740360289052}, {'factory': '南', 'magnitude': 0.0003714193582435097}]}
 earthEqake_test2 = {'time': '2023-5-12 03:40:52', 'M_L': 3.6, 'focal_dep': 3.2, 'longitude': 41.0, 'latitude': 20.7, 'magnitude': [{'factory': '竹', 'magnitude': 0.00032644588703523386}, {'factory': '中', 'magnitude': 0.00019905740360289052}, {'factory': '南', 'magnitude': 0.0003714193582435097}]}
-electricity_test = {'region':"北", 'power_usage':512.3, 'power_generate': 482.1, 'time': "2023-5-12 03:40:52"}
+electricity_test = {'region':"竹", 'power_usage':512.3, 'power_generate': 482.1, 'time': "2023-5-12 03:40:52"}
 electricity_test1 = {'region':"中", 'power_usage':512.3, 'power_generate': 482.1, 'time': "2023-5-12 03:40:52"}
 electricity_test2 = {'region':"南", 'power_usage':510.3, 'power_generate': 472.1, 'time': "2023-5-12 03:40:52"}
 reservoir_test1 = {'time':"2023-5-12 03:40:52", 'percentage': 427.6, 'water_supply': 321.2, 'name': "鯉魚潭水庫"}
