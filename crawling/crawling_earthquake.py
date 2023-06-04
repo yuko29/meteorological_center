b718adce2a83686d250987e06921b4a17fa8c964
vivian96385 <70514684+vivian96385@users.noreply.github.com>
2023-06-04 11:24:27 +0800

    fix wrong magnitude, stdout log

--- crawling/crawling_earthquake.py
+++ crawling/crawling_earthquake.py
@@ -2,14 +2,23 @@ import requests
 import re
 import math
 from math import sin, cos, radians
+from datetime import datetime,timezone,timedelta
 from dbAPI.MongoDB import MongoDB
 
+def get_now_time():
+    dtUTC = datetime.utcnow().replace(tzinfo = timezone.utc) #UTC
+    dtUTC_8 = dtUTC.astimezone(timezone(timedelta(hours = 8))) #UTC+8
+    now_time = dtUTC_8.strftime("%Y-%m-%d %H:%M:%S")
+    
+    return now_time
 
 def get_history(url: str):
     
     try:
         earthquake = requests.get(url) 
-    except requests.exceptions.RequestException:
+    except requests.exceptions.RequestException as e:
+        print("[crawling] ", e)
+        print("[crawling]  Quit crawling...")
         exit()
     
     html_content = earthquake.content.decode('utf-8')
@@ -22,15 +31,10 @@ def get_history(url: str):
 
 def crawl_data(url: str):
     history = get_history(url)
-    history={'time':history[0][2], 'M_L':history[0][3], 'focal_dep': history[0][4], 'longitude': float(history[0][7]), 'latitude': float(history[0][8])}
+    history = {'time':history[0][2], 'M_L':history[0][3], 'focal_dep': history[0][4], 'longitude': float(history[0][7]), 'latitude': float(history[0][8])}
+    print("[crawling]  Crawled data: ", history)
 
     return history
-
-
-def crawl_data(url: str):
-    history = get_history(url)
-    earthQuake = {'time':history[0][2], 'M_L':history[0][3], 'focal_dep': history[0][4], 'longitude': float(history[0][7]), 'latitude': float(history[0][8])}
-    return earthQuake
     
 def longitude_difference_to_km(longitude1: float, longitude2: float):
     """
@@ -102,19 +106,34 @@ def calculate_magnitude(data: dict, GG_factory: list):
         padj = fac['Padj']
         PGA = 1.657*math.exp(1.533*data['M_L'])*( r **(-1.607))*fac['Si']*fac['Padj']
         PGV = PGA/8.6561
-        if PGA>80:
-            #print(PGV)
-            fac_magnitude.append( {"factory":fac['factory'], "magnitude":PGV})
+        if PGA > 80:
+            # PGV
+            if PGV < 30:
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 5})
+            elif PGV < 50:
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 5.5})
+            elif PGV < 80:
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 6})
+            elif PGV < 140:
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 6.5})
+            else:
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 7})
         else:
-            #print(PGA)
-            fac_magnitude.append({"factory":fac['factory'], "magnitude":PGA})
+            # PGA
+            if PGA < 0.2:
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 0})
+            elif PGA < 0.7:
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 1})
+            elif PGA < 1.9:
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 2})
+            elif PGA < 5.7:
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 3})
+            else:
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 4})
 
     data['magnitude'] = fac_magnitude
     return(data)
 
-def get_new_update(url: str):
-    crawled = crawl_data(url)
-    eq_with_mag = calculate_magnitude(crawled)
 
 def main():
     url = "https://scweb.cwb.gov.tw/"
@@ -124,16 +143,20 @@ def main():
     {'factory': '南', 'longitude': 120.272, 'latitude': 23.1135, 'Si': 1.968, 'Padj': 1.0, 'magnitude': []}
     ]
     
+    now_time = get_now_time()
+    print("------------------")
+    print("[crawling]  Start crawling water, time:", now_time)
+    
     earthQuake = crawl_data(url)
-
     earthQuake = calculate_magnitude(earthQuake, GG_factory)
     
-    print(earthQuake)
     a = MongoDB()
+    #a = MongoDB(ip = "172.27.0.1", port = 27017)
     a.insert_earthquake_data(earthQuake)
+    print("[crawling]  Insert data: ", earthQuake)
     
-    print(f"\n\n\nRETRIEVING EARTHQUAKE...\n\n")
-    for i in a.retrieve_earthquake_data(10):
+    print(f"\n\n\nRETRIEVING EARTHQUAKE FOR FACTORY...\n\n")
+    for i in a.retrieve_earthquake_data_by_factory(factory = "竹", quantity = 10):
         print(i)
     
 if __name__ == "__main__":
