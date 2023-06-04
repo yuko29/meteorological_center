489d51c51811d9f06495ed2e3d4f253abaae4769
vivian96385 <70514684+vivian96385@users.noreply.github.com>
2023-06-04 11:31:50 +0800

    stdout log

--- crawling/crawling_elec.py
+++ crawling/crawling_elec.py
@@ -1,11 +1,22 @@
 import requests
 import re
+from datetime import datetime,timezone,timedelta
 from dbAPI.MongoDB import MongoDB
+
+def get_now_time():
+    dtUTC = datetime.utcnow().replace(tzinfo = timezone.utc) #UTC
+    dtUTC_8 = dtUTC.astimezone(timezone(timedelta(hours = 8))) #UTC+8
+    now_time = dtUTC_8.strftime("%Y-%m-%d %H:%M:%S")
+    
+    return now_time
 
 def get_elec(url: str):
     try:
         elec = requests.get(url) #將此頁面的HTML GET下來
-    except requests.exceptions.RequestException:
+        print("[crawling]  Electricity crawled")
+    except requests.exceptions.RequestException as e:
+        print("[crawling] ", e)
+        print("[crawling]  Quit crawling...")
         exit()
     
     return elec
@@ -28,26 +39,39 @@ def handle_data(elec):
 
     return update_time, Supply, Usage
 
-def insert_to_db(update_time:str, Supply: list[float], Usage: list[float]):
+def insert_to_db(update_time:str, Supply: list, Usage: list):
     
     Electricity_north = {'region':"北", 'power_usage': Usage[0], 'power_generate': Supply[0], 'time': update_time}
     Electricity_center = {'region':"中", 'power_usage': Usage[1], 'power_generate': Supply[1], 'time': update_time}
     Electricity_south = {'region':"南", 'power_usage': Usage[2], 'power_generate': Supply[2], 'time': update_time}
     
     a = MongoDB()
+    #a = MongoDB(ip = "172.27.0.1", port = 27017)
     a.insert_electricity_data(Electricity_north)
+    print("[crawling]  Insert data: ", Electricity_north)
     a.insert_electricity_data(Electricity_center)
+    print("[crawling]  Insert data: ", Electricity_center)
     a.insert_electricity_data(Electricity_south)
-    
+    print("[crawling]  Insert data: ", Electricity_south)
+
+    print(f"\n\n\nRETRIEVING ELECTRICITY...\n\n")
+    for i in a.retrieve_electricity_data_by_region(quantity= 50,region="北"):
+        print(i)
     #print(Electricity_north)
     #print(Electricity_center)
     #print(Electricity_south)
 
 def main():
     url = "https://www.taiwanstat.com/powers/latest/"
+    
+    now_time = get_now_time()
+    print("------------------")
+    print("[crawling]  Start crawling electricity, time:", now_time)
+    
     elec = get_elec(url)
     update_time, Supply, Usage = handle_data(elec)
     insert_to_db(update_time, Supply, Usage)
+    
     
 if __name__ == "__main__":
     main()
