60c4d592005bb50a600c1c76fe6d2a457235ec9e
vivian96385 <70514684+vivian96385@users.noreply.github.com>
2023-06-04 10:56:33 +0800

    reject insert incomplete data, stdout log message added

--- crawling/crawling_water.py
+++ crawling/crawling_water.py
@@ -1,54 +1,67 @@
 from bs4 import BeautifulSoup
 import requests
+from datetime import datetime,timezone,timedelta
 from dbAPI.MongoDB import MongoDB
 
 def get_water(url: str):
     try:
         water = requests.get(url) #將此頁面的HTML GET下來
-    except requests.exceptions.RequestException:
+    except requests.exceptions.RequestException as e:
+        print("[crawling] ", e)
+        print("[crawling]  Quit crawling...")
         exit()
     soup = BeautifulSoup(water.text, 'html.parser')
     return soup
 
-def handle_data(soup, re_list: list[str]):
-    reservoir = {} # key: reservoir name, value: a dict of information
-    
+def handle_data(a, soup, re_list: list):
     for re in re_list:
         stw = soup.find('a', string = re).find_parent().find_parent().find_all("td")
-        
+    
         if stw[1].string == "--":
-            water_time = '-'
+            print("[crawling] ", re, " no data crawled")
+            continue
         else:
             water_time = str(stw[1].string)
         
         if stw[6].string == "--":
-            water_avail = -1.0
+            print("[crawling] ", re, " no data crawled")
+            continue
         else:
             water_avail = float(stw[6].string.replace(',',''))
         
         if stw[7].string == "--":
-            water_per = -1.0
+            print("[crawling] ", re, " no data crawled")
+            continue
         else:
             water_per = str(stw[7].string).replace(" %","")
             water_per = float(water_per)
         
-        reservoir[re] = {"time": water_time, "volume": water_avail, "percentage": water_per}
-    
-    return reservoir
-
-def insert_to_db(reservoir: dict, re_list: list[str]):
-    a = MongoDB()
-    for re in re_list:
-        a.insert_reservoir_data({'time':reservoir[re]['time'], 'percentage': reservoir[re]['percentage'], 'water_supply': reservoir[re]['volume'], 'name': re})
-        #print(re, reservoir[re])
+        reservoir_data = {'name': re, "time": water_time, "water_supply": water_avail, "percentage": water_per}
+        a.insert_reservoir_data(reservoir_data)
+        
+        print("[crawling] ", re, " insert data ", reservoir_data)
+    return 
 
 def main():
     url = "https://fhy.wra.gov.tw/ReservoirPage_2011/Statistics.aspx"
     re_list = ["石門水庫", "寶山第二水庫", "永和山水庫", "鯉魚潭水庫", "德基水庫", "南化水庫", "曾文水庫", "烏山頭水庫"]
     
+    dtUTC = datetime.utcnow().replace(tzinfo = timezone.utc) #UTC
+    dtUTC_8 = dtUTC.astimezone(timezone(timedelta(hours = 8))) #UTC+8
+    now_time = dtUTC_8.strftime("%Y-%m-%d %H:%M:%S")
+    print("------------------")
+    print("[crawling]  Start crawling water, time:", now_time)
+
     soup = get_water(url)
-    reservoir = handle_data(soup, re_list)
-    insert_to_db(reservoir, re_list)
+    #a = MongoDB()
+    a = MongoDB(ip = "172.27.0.1", port = 27017)
+    handle_data(a, soup, re_list)
+    
+    print()
+    print("Retrieve data")
+    print(a.retrieve_reservoir_data_by_name(quantity= 50, name = "永和山水庫"))
+
+    a.reset()
     
 if __name__ == "__main__":
     main()
