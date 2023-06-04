9724e1d5d3c01e7d48b03cc6de30f58a5cb12b51
vivian96385 <70514684+vivian96385@users.noreply.github.com>
2023-06-04 20:14:09 +0800

    check typing, use try for api call

--- crawling/crawling_earthquake.py
+++ crawling/crawling_earthquake.py
@@ -30,8 +30,11 @@ def get_history(url: str):
     return history
 
 def crawl_data(url: str):
+    
     history = get_history(url)
-    history = {'time':history[0][2], 'M_L':history[0][3], 'focal_dep': history[0][4], 'longitude': float(history[0][7]), 'latitude': float(history[0][8])}
+    format_time = datetime.strptime(history[0][2], "%Y-%m-%d %H:%M:%S")
+    print("format_time: ", format_time)
+    history = {'time':format_time, 'M_L':history[0][3], 'focal_dep': history[0][4], 'longitude': float(history[0][7]), 'latitude': float(history[0][8])}
     print("[crawling]  Crawled data: ", history)
 
     return history
@@ -109,31 +112,47 @@ def calculate_magnitude(data: dict, GG_factory: list):
         if PGA > 80:
             # PGV
             if PGV < 30:
-                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 5})
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 5.0})
             elif PGV < 50:
                 fac_magnitude.append( {"factory": fac['factory'], "magnitude": 5.5})
             elif PGV < 80:
-                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 6})
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 6.0})
             elif PGV < 140:
                 fac_magnitude.append( {"factory": fac['factory'], "magnitude": 6.5})
             else:
-                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 7})
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 7.0})
         else:
             # PGA
             if PGA < 0.2:
-                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 0})
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 0.0})
             elif PGA < 0.7:
-                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 1})
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 1.0})
             elif PGA < 1.9:
-                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 2})
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 2.0})
             elif PGA < 5.7:
-                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 3})
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 3.0})
             else:
-                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 4})
+                fac_magnitude.append( {"factory": fac['factory'], "magnitude": 4.0})
 
     data['magnitude'] = fac_magnitude
     return(data)
 
+def insert_to_db(time: str, M_L: float, focal_dep: float, longitude: float, latitude: float, magnitude_chu: float, magnitude_center: float, magnitude_south: float):
+    a = MongoDB()
+    #a = MongoDB(ip = "172.27.0.1", port = 27017)
+  
+    try:
+        a.insert_earthquake_data({'time': time, 'M_L': M_L, 'focal_dep': focal_dep, 'longitude': longitude, 'latitude': latitude, 'magnitude': [{'factory': '竹', 'magnitude': magnitude_chu}, {'factory': '中', 'magnitude': magnitude_center}, {'factory': '南', 'magnitude': magnitude_south}]})
+        print("[crawling]  Insert data: ", {'time': time, 'M_L': M_L, 'focal_dep': focal_dep, 'longitude': longitude, 'latitude': latitude, 'magnitude': [{'factory': '竹', 'magnitude': magnitude_chu}, {'factory': '中', 'magnitude': magnitude_center}, {'factory': '南', 'magnitude': magnitude_south}]})
+    except:
+        print("[crawling]  Insert data failed")
+    
+    # print(f"\n\n\nRETRIEVING EARTHQUAKE FOR FACTORY...\n\n")
+    # for i in a.retrieve_earthquake_data_by_factory(factory = "竹", quantity = 10):
+    #     print(i)
+    #     print(type(i['time']))
+    
+    return
 
 def main():
     url = "https://scweb.cwb.gov.tw/"
@@ -145,19 +164,16 @@ def main():
     
     now_time = get_now_time()
     print("------------------")
-    print("[crawling]  Start crawling earthquake, time:", now_time)
+    print("[crawling]  Start crawling water, time:", now_time)
     
     earthQuake = crawl_data(url)
     earthQuake = calculate_magnitude(earthQuake, GG_factory)
     
-    a = MongoDB()
-    #a = MongoDB(ip = "172.27.0.1", port = 27017)
-    a.insert_earthquake_data(earthQuake)
-    print("[crawling]  Insert data: ", earthQuake)
+
+    insert_to_db(earthQuake['time'], earthQuake['M_L'], earthQuake['focal_dep'], earthQuake['longitude'] \
+    , earthQuake['latitude'], earthQuake['magnitude'][0]["magnitude"], earthQuake['magnitude'][1]["magnitude"] \
+    , earthQuake['magnitude'][2]["magnitude"])
     
-    print(f"\n\n\nRETRIEVING EARTHQUAKE FOR FACTORY...\n\n")
-    for i in a.retrieve_earthquake_data_by_factory(factory = "竹", quantity = 10):
-        print(i)
     
 if __name__ == "__main__":
     main()
